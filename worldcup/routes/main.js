1cb6f3a57ce233e5841d2736d058f26974759298
westmini427 <seominhui427@gmail.com>
2023-06-20 08:36:46 +0000

    docker final

--- worldcup/routes/main.js
+++ worldcup/routes/main.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const mongoClient = require('mongodb').MongoClient;
@@ -9,45 +9,45 @@ const app = express()
 app.use(morgan('dev'));
 
 var db;
-var databaseUrl = 'mongodb://15.165.178.2:27017';
+var databaseUrl = 'mongodb://43.201.211.135:27017';
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './index.html');
-});
+	res.sendFile(__dirname + './index.html');
+})
 
-app.get("/worldcup", (req, res) => {
-    console.log(mongoClient);
-    mongoClient.connect(databaseUrl, function(err, database) {
-        if (err != null) {
-                res.json({'count':0});
-                console.log(err);
-    } else {
-        db.database.db('test');
-        db.collection('worldcup').find({}, {_id:0, no:1, nation:1, date:1}).toArray(function(err, result) {
-                if (err) throw err;
-                console.log('result: ');
-                console.log(result);
-                res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
-                var template = `
-                        <table border="1" margin:auto; text-align:center;>
-                        <tr>
-                                <th> No </th>
-                                <th> Nation </th>
-                                <th> Date </th>
-                        </tr> `;
-                result.forEach((item) => {
-                        template += `
-                        <tr>
-                                <th> ${item.no} </th>
-                                <th> ${item.nation} </th>
-                                <th> ${item.date} </th>
-                        </tr> `;
-                });
-                template += `</table>`;
-                res.end(template);
-                });
-        }
-    });
+app.get('/worldcup', (req, res) => {
+	console.log(mongoClient)
+	mongoClient.connect(databaseUrl, function(err, database) {
+	if (err != null) {
+		res.json({'count':0});
+		console.log(err);
+	} else {	
+		db = database.db('test');
+		db.collection('worldcup').find({}, {_id:0, no:1, nation:1, date:1}).toArray(function(err, result) {
+				if (err) throw err;
+				console.log('result: ');
+				console.log(result);
+				res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+				var template = `
+					<table boarder="1" margin:auto; text-align:center;>
+					<tr>
+						<th> No </th>
+						<th> Nation </th>
+						<th> Date </th>
+					</tr> `;
+					result.forEach((item) => {
+						template += `
+						<tr>
+							<th> ${item.no} </th>
+							<th> ${item.nation} </th>
+							<th> ${item.date} </th>
+						</tr> `;
+					});
+					template += `</table>`;
+				res.end(template);
+			});
+		}
+	});
 });
 
 module.exports = app;
