654d079f72d272cb43c501dc4e7af99c987715e9
westmini427 <seominhui427@gmail.com>
2023-06-20 09:29:19 +0000

    docker final

--- worldcup/routes/main.js
+++ worldcup/routes/main.js
@@ -29,7 +29,7 @@ app.get('/worldcup', (req, res) => {
 				console.log(result);
 				res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
 				var template = `
-					<table boarder="1" margin:auto; text-align:center;>
+					<table border="1" margin:auto; text-align:center;>
 					<tr>
 						<th> No </th>
 						<th> Nation </th>
